1bd33b329b195b62fc6c175300e488152472df39
Aditya Goyal <adi@Adityas-MacBook-Pro.local>
2018-02-19 21:34:52 +1100

    ini vue project

---
+++ client/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <PageHeader/>
+    <img src="./assets/logo.png">
+    <Container/>
+  </div>
+</template>
+
+<script>
+import Container from './components/Container'
+import PageHeader from './components/PageHeader'
+
+export default {
+  name: 'App',
+  components: {
+    Container,
+    PageHeader
+  }
+}
+</script>
+
+<style lang="scss">
+ #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+</style>
---
+++ client/src/components/PageHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-header">
+    adventure<span>guide</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.page-header{
+  width: 100%;
+  height: 48px;
+  background: orange;
+  color:white;
+  span {
+    color:black;
+  }
+}
+</style>
